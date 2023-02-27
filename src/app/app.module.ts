4bf780bef5b2ea86f4698c8c7d735c6d2e1c4368
valondamn <69294985+valondamn@users.noreply.github.com>
2023-02-27 16:30:17 +0600

    Удалил папку example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExampleComponent } from './example/example.component';
+
 import { HeaderComponent } from './header/header.component';
 import { VideoBlockComponent } from './video-block/video-block.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleComponent,
+
     HeaderComponent,
     VideoBlockComponent
   ],
