c4e8eca924c740707f892dad9a7a52f73c058a65
valondamn <69294985+valondamn@users.noreply.github.com>
2023-02-28 16:21:52 +0600

    Добавил второй блок с видео

--- src/app/video-block/video-block.component.ts
+++ src/app/video-block/video-block.component.ts
@@ -1,19 +1,59 @@
 import { Component } from '@angular/core';
-interface investor{
-name:any
-title?:any[]
+import { Title } from '@angular/platform-browser';
+interface investor { 
+  name: any 
+  title?: any[]   
+  name2:  any
+  title2: any[]
 }
+
+interface block_video{ 
+  header: any 
+  
+  sub_header: any 
+  
+}
+
+
+
 
 @Component({
   selector: 'app-video-block',
   templateUrl: './video-block.component.html',
   styleUrls: ['./video-block.component.scss']
 })
-export class VideoBlockComponent {
-investors:investor[]=[{
-  name:'Инвестируя в IPO на KASE, у вас появляется возможность:',
-title:[{
-  info:'стать совладельцем успешных, стабильно развивающихся казахстанских компаний, выходящих на IPO',
-  info1:'получить доступ к другим инструментам, торгуемым на KASE'
-}]}]
+export class VideoBlockComponent { 
+
+  block_video: block_video[] = [{
+    header: 'Зачем инвестировать в IPO на KASE?', 
+    sub_header: "KASE - ваш партнер уверенного роста!", 
+   
+  }]
+
+
+  
+
+
+
+
+
+  investors: 
+  investor[] = [{
+    name: 'Инвестируя в IPO на KASE, у вас появляется возможность:',  
+    name2: 'Эти возможности позволят вам:',
+    title: [{
+      info: 'стать совладельцем успешных, стабильно развивающихся казахстанских компаний, выходящих на IPO',
+      info1: 'получить доступ к другим инструментам, торгуемым на KASE'}],  
+ 
+    title2:[{
+      info2:"создать источник дополнительного дохода", 
+      info3:"сформировать капитал на долгосрочные цели (образование, пенсия)", 
+      info4:"внести свой вклад в развитие бизнеса отдельных компаний и экономики страны"
+    }]
+    
+  }]
+
+  
+  
 }
+
